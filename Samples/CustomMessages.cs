1d908943efdb6c35faf56ce4c5f80a42228ec343
bluetrain <36786006+blue-train@users.noreply.github.com>
2023-12-27 18:08:02 +0100

    Add message examples

---
+++ Samples/CustomMessages.cs
@@ -0,0 +1,8 @@
+namespace BlueTrain.MessageDispatching.Samples
+{
+    public class GameStarted : MessageBase { }
+
+    public class WorldLoaded : MessageBase { }
+
+    public class SavingGameRequested : MessageBase { }
+}
