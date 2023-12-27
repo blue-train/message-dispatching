f563cbb00aa405e774c3d793ba92125f2061a8d8
bluetrain <36786006+blue-train@users.noreply.github.com>
2023-12-27 18:48:45 +0100

    Unity 2019.4 compatibility

--- Runtime/MessageDispatcher.cs
+++ Runtime/MessageDispatcher.cs
@@ -5,7 +5,7 @@ namespace BlueTrain.MessageDispatching
 {
     public static class MessageDispatcher
     {
-        private static readonly Dictionary<Type, MessageHandler> MessageHandlers = new();
+        private static readonly Dictionary<Type, MessageHandler> MessageHandlers = new Dictionary<Type, MessageHandler>();
 
         public static void Subscribe<TMessage>(MessageHandler handler) where TMessage : MessageBase
         {
